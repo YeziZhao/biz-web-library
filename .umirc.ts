f43a063dca07bf206c6194cbde7705a8aeaee13d
zhaoyehong <zhao.yehong@navercorp.com>
2020-12-07 11:08:52 +0800

    doc test

--- .umirc.ts
+++ .umirc.ts
@@ -8,11 +8,11 @@ export default defineConfig({
   logo: '/images/photos.jpg',
   description: '用于Biz Web Dev 前端组件开发。',
   // resolve: { includes: ['document']},
-  base: '/biz-web-library',
-  publicPath: '/biz-web-library/',
-  exportStatic: {
-    htmlSuffix: true,
-  }, // 对每隔路由输出html
+  base: '/docs-dist/',
+  publicPath: '/biz-web-library/', // 生成路径
+  // exportStatic: {
+  //   htmlSuffix: true,
+  // }, // 对每隔路由输出html
   locales: [
     ['en-US', 'English'],
     ['zh-CN', '中文'],
